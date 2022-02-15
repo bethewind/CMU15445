1dffaeff4ac78ef907baf8f20edf671a6dd0cc8d
daohwang <1760730301@qq.com>
2022-02-15 15:19:16 +0800

    pass update executor

--- src/execution/update_executor.cpp
+++ src/execution/update_executor.cpp
@@ -9,17 +9,32 @@
 // Copyright (c) 2015-20, Carnegie Mellon University Database Group
 //
 //===----------------------------------------------------------------------===//
+#include <cstdio>
 #include <memory>
 
+#include "catalog/catalog.h"
 #include "execution/executors/update_executor.h"
 
 namespace bustub {
 
 UpdateExecutor::UpdateExecutor(ExecutorContext *exec_ctx, const UpdatePlanNode *plan,
                                std::unique_ptr<AbstractExecutor> &&child_executor)
-    : AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx), plan_(plan), table_info_(nullptr), child_executor_(std::move(child_executor)) {}
 
-void UpdateExecutor::Init() {}
+void UpdateExecutor::Init() {
+  table_info_ = exec_ctx_->GetCatalog()->GetTable(plan_->TableOid());
+  child_executor_->Init();
+}
 
-bool UpdateExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) { return false; }
+bool UpdateExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) {
+  Tuple tmp_tuple;
+  RID tmp_rid;
+  if (!child_executor_->Next(&tmp_tuple, &tmp_rid)) {
+    return false;
+  }
+  Tuple new_tuple = GenerateUpdatedTuple(tmp_tuple);
+  table_info_->table_->UpdateTuple(new_tuple, tmp_rid, exec_ctx_->GetTransaction());
+  *rid = tmp_rid;
+  return true;
+}
 }  // namespace bustub
